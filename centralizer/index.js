d47a8a80df133104e7b6bf2c32664bd4b06ae8ff
andyNroses <pages.andy@hotmail.fr>
2018-09-28 23:12:40 +0100

    Fix

--- centralizer/index.js
+++ centralizer/index.js
@@ -1,17 +1,2 @@
 $(document).ready(function() {
-    $('.shape').shape();
-    const links = ['email', 'timetable', 'moodle', 'portal'];
-    for (var i in links) {
-      const selector = '.shape.' + links[i];
-      $(selector).on('mouseenter', (e) => {
-        if (!$(selector).shape('is animating')) {
-          $(selector).shape('flip over');
-        }
-      });
-      $(selector).on('mouseleave', () => {
-        if (!$(selector).shape('is animating')) {
-          $(selector).shape('flip over');
-        }
-      });
-    }
 });
